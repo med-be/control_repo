01c0e20e71e1004bf0e34b2b59b03b45e7f0280c
Mohamed El Khanche <79124137+med-be@users.noreply.github.com>
2022-09-06 21:38:09 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,7 +7,7 @@ class minecraft {
     ensure => file,
     source => 'https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar',
   }
-  file {'java':
+  package {'java':
     ensure => psesent,
   }
   file {'/opt/minecraft/eula.txt':
